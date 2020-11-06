c00bfb8e37039df7904cf3f35f40315e99f96ee4
Hunter Read <hunter.read@discorp.com>
2020-11-06 12:03:43 -0800

    regex fix for games and new test case

--- text/game_parser.py
+++ text/game_parser.py
@@ -1,7 +1,7 @@
 import re
 
 
-__game_regex = re.compile(r"(CoC|3.5|[3-5]e|PF[1-2]e|BitD|BRP|CofD|Cyberpunk|DLC|DLR|DCC|DW|ODND|ADND|BX|DND2e|Earthdawn|Fate|Feast|FWS|GURPS|L5R|MCC|MotW|MM3|Numenera|SWADE|SWD|SR[3-6]|Starfinder|SWRPG|SWN|40K|WoD)", flags=re.IGNORECASE)
+__game_regex = re.compile(r"(CoC|3\.5|[3-5]e|PF[1-2]e|BitD|BRP|CofD|Cyberpunk|DLC|DLR|DCC|DW|ODND|ADND|BX|DND2e|Earthdawn|Fate|Feast|FWS|GURPS|L5R|MCC|MotW|MM3|Numenera|SWADE|SWD|SR[3-6]|Starfinder|SWRPG|SWN|40K|WoD)", flags=re.IGNORECASE)
 
 
 def parse_game(text):
