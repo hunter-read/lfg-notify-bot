3e2f8deec7bffe5058ad7368cf4c9041e1c85017
Hunter Read <hunter.read@discorp.com>
2020-11-19 11:54:59 -0800

    incoming message bot refactor and testing

--- src/incoming_message_bot.py
+++ src/incoming_message_bot.py
@@ -13,64 +13,64 @@ __logger: logging.Logger = logging.getLogger("message_bot")
 
 def read_messages(db: Database):
     for message in __reddit.inbox.stream():
-
-        user = UserRequest()
-        user.username = message.author.name
-
         message.mark_read()
-        full_message = message.subject + message.body
-        __logger.info(f"New Message: {message.author.name} - {message.subject}")
+        reply = parse_incoming_message(db, message)
+        message.reply(reply)
+        time.sleep(2)
 
-        if re.search(r'stop', full_message, re.IGNORECASE) or re.search(r"unsubscribe", full_message, re.IGNORECASE):
-            user.delete(db)
-            message.reply(body=("You have successfully stopped notifications from LFG Notify Bot.  \n"
-                                "If this bot was helpful, please consider making a donation to charity or your GM."))
-            time.sleep(2)
 
-        elif re.search(r'sub(?:scribe)?', message.subject, re.IGNORECASE) or re.search(r'notify', message.subject, re.IGNORECASE) or re.search(r'lfg', message.subject, re.IGNORECASE) or parse_game(message.subject):
-            game = parse_game(full_message)
-            if not game:
-                message.reply(body=("You must include a valid game from the LFG subreddit game tags list https://www.reddit.com/r/lfg/wiki/index/formatting#wiki_game_tags.  \n"
-                                    "Examples include 5e, CoC, GURPS, or PF1e. Other and Flexible LFG tags are not currently supported.  \n"
-                                    "&nbsp;  \n"
-                                    "^^For ^^error ^^reporting, ^^please ^^message ^^u/Perfekthuntr."))
-                continue
+def parse_incoming_message(db: Database, message: praw.models.Message) -> str:
+    user = UserRequest()
+    user.username = message.author.name
 
-            user.game = game
-            user.days = parse_day(message.body)
+    full_message = message.subject + message.body
+    __logger.info(f"New Message: {message.author.name} - {message.subject}")
 
-            timezone = parse_timezone(message.body)
-            output = []
-            if timezone:
-                corrected = set([timezone_to_gmt(tz) for tz in timezone])
-                output = [f"{tz} ({timezone_to_gmt(tz)})" for tz in timezone]
-                user.timezone = corrected
+    if re.search(r'(stop|unsubscribe)', full_message, re.IGNORECASE):
+        user.delete(db)
+        return ("You have successfully stopped notifications from LFG Notify Bot.  \n"
+                "If this bot was helpful, please consider making a donation to charity or your GM.")
 
-            user.nsfw = is_nsfw(message.body)
+    elif re.search(r'(bug|issue|error|feature|suggestion)', full_message, re.IGNORECASE):
+        return "For error reporting or feature requests, please message u/Perfekthuntr."
 
-            user.save(db)
+    elif re.search(r'(sub(?:scribe)?|notify|lfg(?!\spost))', message.subject, re.IGNORECASE) or parse_game(message.subject):
+        game = parse_game(full_message)
+        if not game:
+            return ("You must include a valid game from the LFG subreddit game tags list https://www.reddit.com/r/lfg/wiki/index/formatting#wiki_game_tags.  \n"
+                    "Examples include 5e, CoC, GURPS, or PF1e. Other and Flexible LFG tags are not currently supported.  \n"
+                    "&nbsp;  \n"
+                    "^^For ^^error ^^reporting, ^^please ^^message ^^u/Perfekthuntr.")
 
-            message.reply(body=(f"You have been successfully subscribed to LFG Notify Bot.  \n"
-                                "&nbsp;  \n"
-                                "Your current settings are:  \n"
-                                f"- Game(s): {', '.join(user.game)}  \n"
-                                f"- Timezone(s): {', '.join(output) if output else 'None Input'}  \n"
-                                f"- Day(s) of the week: {', '.join(sort_days(user.days)) if user.days else 'None Input'}  \n"
-                                f"- Include NSFW: {'No' if user.nsfw == 0 else 'Yes'}  \n"
-                                "&nbsp;  \n"
-                                "If you wish to change these settings, reply to this message, or reply **STOP** to end notifications.  \n"
-                                "&nbsp;  \n"
-                                "^^For ^^error ^^reporting, ^^please ^^message ^^u/Perfekthuntr."))
-            time.sleep(2)
+        user.game = game
+        user.nsfw = is_nsfw(message.body)
+        user.days = parse_day(message.body)
+        timezone = parse_timezone(message.body)
+        output = []
+        if timezone:
+            corrected = set([timezone_to_gmt(tz) for tz in timezone])
+            output = [f"{tz} ({timezone_to_gmt(tz)})" for tz in timezone]
+            user.timezone = corrected
 
-        elif re.search(r'bug', message.subject, re.IGNORECASE) or re.search(r'issue', message.subject, re.IGNORECASE) or re.search(r'error', message.subject, re.IGNORECASE):
-            message.reply(body="For error reporting, please message u/Perfekthuntr.")
+        user.save(db)
 
-        else:
-            message.reply(body=("Unknown message sent. If you wish to subscribe to this bot, please send a new message titled 'Subscribe' with your options in the body of the message.  \n"
-                                "If you wish to end notifications reply **STOP** to any message, or send a new message titled 'Stop'.  \n"
-                                "&nbsp;  \n"
-                                "^^For ^^error ^^reporting, ^^please ^^message ^^u/Perfekthuntr."))
+        return (f"You have been successfully subscribed to LFG Notify Bot.  \n"
+                "&nbsp;  \n"
+                "Your current settings are:  \n"
+                f"- Game(s): {', '.join(user.game)}  \n"
+                f"- Timezone(s): {', '.join(output) if output else 'None Input'}  \n"
+                f"- Day(s) of the week: {', '.join(sort_days(user.days)) if user.days else 'None Input'}  \n"
+                f"- Include NSFW: {'No' if user.nsfw == 0 else 'Yes'}  \n"
+                "&nbsp;  \n"
+                "If you wish to change these settings, reply to this message, or reply **STOP** to end notifications.  \n"
+                "&nbsp;  \n"
+                "^^For ^^error ^^reporting, ^^please ^^message ^^u/Perfekthuntr.")
+
+    else:
+        return ("Unknown message sent. If you wish to subscribe to this bot, please send a new message titled 'Subscribe' with your options in the body of the message.  \n"
+                "If you wish to end notifications reply **STOP** to any message, or send a new message titled 'Stop'.  \n"
+                "&nbsp;  \n"
+                "^^For ^^error ^^reporting, ^^please ^^message ^^u/Perfekthuntr.")
 
 
 def init_logger() -> None:
