65b60ba69bde7f6151daaf6f2f4ba38f1cd4aef3
Hunter Read <hunter.read@discorp.com>
2021-05-12 14:05:33 -0700

    Web page clarity improvements

--- web/script.js
+++ web/script.js
@@ -83,7 +83,7 @@ const App = {
     ],
     selectedKeywords: [],
     keywords: [],
-    selectedFlair: ['gmplw', 'plw'],
+    selectedFlair: [],
     flairs: [
       {label: 'GM and player(s) wanted', value: 'gmplw'},
       {label: 'Player(s) wanted', value: 'plw'},
